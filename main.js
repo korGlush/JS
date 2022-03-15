10a37630b0ebef23384b244ae6b5cb220836b876
Glush <90596091+korGlush@users.noreply.github.com>
2022-03-15 11:53:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let title = "javaScript";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 4535;
+let rollback = 45;
+let fullPrice = 453453;
+let adaptive = false;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей" + " Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase);
+
+console.log(screens.split(","));
+
+console.log((fullPrice * (rollback/100)));
+
+alert("ahert");
+console.log("wow");
+
